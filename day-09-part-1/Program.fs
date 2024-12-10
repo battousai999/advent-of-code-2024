ee13444bb16c92c8a51430ae75d59b90c7d34883
Kurtis Jones <kurtis.jones@extendhealth.com>
2024-12-09 17:03:19 -0700

    Implement day 9 problems

--- day-09-part-1/Program.fs
+++ day-09-part-1/Program.fs
@@ -1,2 +1,105 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// Another push of the button leaves you in the familiar hallways of some friendly amphipods! Good thing you each somehow got your own personal mini submarine. The Historians jet away in search of the Chief, mostly by driving directly into walls.
+//
+// While The Historians quickly figure out how to pilot these things, you notice an amphipod in the corner struggling with his computer. He's trying to make more contiguous free space by compacting all of the files, but his program isn't working; you offer to help.
+//
+// He shows you the disk map (your puzzle input) he's already generated. For example:
+//
+// 2333133121414131402
+//
+// The disk map uses a dense format to represent the layout of files and free space on the disk. The digits alternate between indicating the length of a file and the length of free space.
+//
+// So, a disk map like 12345 would represent a one-block file, two blocks of free space, a three-block file, four blocks of free space, and then a five-block file. A disk map like 90909 would represent three nine-block files in a row (with no free space between them).
+//
+// Each file on disk also has an ID number based on the order of the files as they appear before they are rearranged, starting with ID 0. So, the disk map 12345 has three files: a one-block file with ID 0, a three-block file with ID 1, and a five-block file with ID 2. Using one character for each block where digits are the file ID and . is free space, the disk map 12345 represents these individual blocks:
+//
+// 0..111....22222
+//
+// The first example above, 2333133121414131402, represents these individual blocks:
+//
+// 00...111...2...333.44.5555.6666.777.888899
+//
+// The amphipod would like to move file blocks one at a time from the end of the disk to the leftmost free space block (until there are no gaps remaining between file blocks). For the disk map 12345, the process looks like this:
+//
+// 0..111....22222
+// 02.111....2222.
+// 022111....222..
+// 0221112...22...
+// 02211122..2....
+// 022111222......
+//
+// The first example requires a few more steps:
+//
+// 00...111...2...333.44.5555.6666.777.888899
+// 009..111...2...333.44.5555.6666.777.88889.
+// 0099.111...2...333.44.5555.6666.777.8888..
+// 00998111...2...333.44.5555.6666.777.888...
+// 009981118..2...333.44.5555.6666.777.88....
+// 0099811188.2...333.44.5555.6666.777.8.....
+// 009981118882...333.44.5555.6666.777.......
+// 0099811188827..333.44.5555.6666.77........
+// 00998111888277.333.44.5555.6666.7.........
+// 009981118882777333.44.5555.6666...........
+// 009981118882777333644.5555.666............
+// 00998111888277733364465555.66.............
+// 0099811188827773336446555566..............
+//
+// The final step of this file-compacting process is to update the filesystem checksum. To calculate the checksum, add up the result of multiplying each of these blocks' position with the file ID number it contains. The leftmost block is in position 0. If a block contains free space, skip it instead.
+//
+// Continuing the first example, the first few blocks' position multiplied by its file ID number are 0 * 0 = 0, 1 * 0 = 0, 2 * 9 = 18, 3 * 9 = 27, 4 * 8 = 32, and so on. In this example, the checksum is the sum of these, 1928.
+//
+// Compact the amphipod's hard drive using the process he requested. What is the resulting filesystem checksum?
+
+open System
+open System.IO
+
+type DiskBlock =
+    | File of int
+    | FreeSpace
+    | Empty
+
+let rawDiskMap = File.ReadAllText "./input.txt"
+
+// let rawDiskMap = "2333133121414131402"
+
+let asDigit (x: char) = int x - int '0'
+
+let diskMap =
+    rawDiskMap + " "
+        |> Seq.chunkBySize 2
+        |> Seq.mapi (fun i xs ->
+            match xs with
+            | [| file; freesize|] -> (i, asDigit file, if freesize = ' ' then None else Some (asDigit freesize))
+            | _ -> raise <| InvalidOperationException "Unexpected input")
+        |> Seq.collect (fun (i, file, freesize) ->
+            let fileBlocks = [ for _ in 1..file -> File i ]
+            let freeBlocks =
+                match freesize with
+                | Some fs -> [ for _ in 1..fs -> FreeSpace ]
+                | None -> []
+            fileBlocks @ freeBlocks)
+        |> Array.ofSeq
+
+let compactDiskMap (diskMap: DiskBlock array) =
+    let mutable i = 0
+
+    while i < diskMap.Length - 1 do
+        match diskMap.[i], diskMap.[i + 1] with
+        | File _, FreeSpace ->
+            let lastDiskBlockIndex = diskMap |> Array.findIndexBack (fun x -> x <> Empty && x <> FreeSpace)
+            diskMap.[i + 1] <- diskMap.[lastDiskBlockIndex]
+            diskMap.[lastDiskBlockIndex] <- Empty
+        | _, Empty -> i <- Int32.MaxValue - 1
+        | _ -> ()
+
+        i <- i + 1
+
+    diskMap |> Array.filter (fun x -> x <> Empty)
+
+let compactedDiskMap = compactDiskMap diskMap
+
+let checksum =
+    compactedDiskMap
+    |> Array.mapi (fun i x -> (int64 i) * (match x with File id -> (int64 id) | _ -> 0))
+    |> Array.sum
+
+printfn "%A" checksum
--- day-09-part-2/Program.fs
+++ day-09-part-2/Program.fs
@@ -1,2 +1,111 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// Upon completion, two things immediately become clear. First, the disk definitely has a lot more contiguous free space, just like the amphipod hoped. Second, the computer is running much more slowly! Maybe introducing all of that file system fragmentation was a bad idea?
+//
+// The eager amphipod already has a new plan: rather than move individual blocks, he'd like to try compacting the files on his disk by moving whole files instead.
+//
+// This time, attempt to move whole files to the leftmost span of free space blocks that could fit the file. Attempt to move each file exactly once in order of decreasing file ID number starting with the file with the highest file ID number. If there is no span of free space to the left of a file that is large enough to fit the file, the file does not move.
+//
+// The first example from above now proceeds differently:
+//
+// 00...111...2...333.44.5555.6666.777.888899
+// 0099.111...2...333.44.5555.6666.777.8888..
+// 0099.1117772...333.44.5555.6666.....8888..
+// 0099.111777244.333....5555.6666.....8888..
+// 00992111777.44.333....5555.6666.....8888..
+//
+// The process of updating the filesystem checksum is the same; now, this example's checksum would be 2858.
+//
+// Start over, now compacting the amphipod's hard drive using this new method instead. What is the resulting filesystem checksum?
+
+open System
+open System.IO
+
+type DiskBlock =
+    | File of int
+    | FreeSpace
+    | Empty
+
+type Section = {
+    Block: DiskBlock
+    Index: int
+    Length: int
+}
+
+let rawDiskMap = File.ReadAllText "../day-09-part-1/input.txt"
+
+// let rawDiskMap = "2333133121414131402"
+
+let asDigit (x: char) = int x - int '0'
+
+let diskMap =
+    rawDiskMap + " "
+        |> Seq.chunkBySize 2
+        |> Seq.mapi (fun i xs ->
+            match xs with
+            | [| file; freesize|] -> (i, asDigit file, if freesize = ' ' then None else Some (asDigit freesize))
+            | _ -> raise <| InvalidOperationException "Unexpected input")
+        |> Seq.collect (fun (i, file, freesize) ->
+            let fileBlocks = [ for _ in 1..file -> File i ]
+            let freeBlocks =
+                match freesize with
+                | Some fs -> [ for _ in 1..fs -> FreeSpace ]
+                | None -> []
+            fileBlocks @ freeBlocks)
+        |> Array.ofSeq
+
+let partitionBlocks (map: DiskBlock array) =
+    let blocks = ResizeArray<Section>()
+    let (x, xs) = (Seq.head map, Seq.tail map)
+    let folder (index: int, starting: int, last: DiskBlock, blocks: ResizeArray<Section>) (current: DiskBlock) =
+        if current <> last then
+            blocks.Add { Block = last; Index = starting; Length = index - starting }
+            (index + 1, index, current, blocks)
+        else
+            (index + 1, starting, last, blocks)
+
+    let (index, starting, last, results) = xs |> Seq.fold folder (1, 0, x, blocks)
+
+    results.Add({ Block = last; Index = starting; Length = index - starting })
+
+    results |> List.ofSeq
+
+let compactDiskMap (diskMap: DiskBlock array) =
+    let partitions = partitionBlocks diskMap
+    let mutable freeSpaces = partitions |> List.filter (fun x -> x.Block = FreeSpace)
+    let files = partitions |> List.filter (fun x -> x.Block <> FreeSpace)
+    let fillBlock destIndex section =
+        let rec innerFill index length =
+            if length > 0 then
+                diskMap[index] <- section.Block
+                innerFill (index + 1) (length - 1)
+
+        innerFill destIndex section.Length
+
+    files
+        |> List.tail
+        |> List.rev
+        |> List.iter
+            (fun file ->
+                let freeSpace = freeSpaces |> List.tryFind (fun x -> x.Length >= file.Length && file.Index > x.Index)
+
+                match freeSpace with
+                | Some fs ->
+                    fillBlock fs.Index file
+                    fillBlock file.Index { file with Block = FreeSpace }
+
+                    if fs.Length > file.Length then
+                        let newFreeSpaceBlock = { Block = FreeSpace; Index = fs.Index + file.Length; Length = fs.Length - file.Length }
+                        freeSpaces <- (newFreeSpaceBlock :: (freeSpaces |> List.filter (fun x -> x <> fs))) |> List.sortBy _.Index
+                    elif fs.Length = file.Length then
+                        freeSpaces <- freeSpaces |> List.filter (fun x -> x <> fs)
+                | None -> ())
+
+    diskMap |> Array.filter (fun x -> x <> Empty)
+
+let compactedDiskMap = compactDiskMap diskMap
+
+let checksum =
+    compactedDiskMap
+    |> Array.mapi (fun i x -> (int64 i) * (match x with File id -> (int64 id) | _ -> 0))
+    |> Array.sum
+
+printfn "%A" checksum
